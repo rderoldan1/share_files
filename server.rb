66a8e12a7afec2136f8f56fd506e18fb7392ced5
Ruben Espinosa <rderoldan1@gmail.com>
2011-10-10 18:07:56 -0500

    first step with file

--- server.rb
+++ server.rb
@@ -3,18 +3,36 @@ require 'socket'
 server = TCPServer.new(2000)
 
 @client={}
+@current = ""
+
+def write(dir, menssage)
+	dir.puts(menssage)
+end
+
+def recv(dir)
+	menssage = dir.gets.chomp
+end
 
 loop do 
 	Thread.start(server.accept) do |connection|
-		connection.puts("Nombre")
-		n = connection.gets.chomp
-		puts n
+		#connection.puts("Nombre")
+		write(connection, "Nombre")
+		#@n = connection.gets.chomp
+		@n = recv(connection)
+		puts @n
 		m = connection.addr
 		puts m
-		@client [n] = connection	
-		connection.puts("Escriba un mensaje")
-		while line = connection.gets
-			
+		@client [@n] = connection	
+		write(connection, "Escriba un mensaje")
+		while line = recv(connection)
+			if (line.eql? 'share')
+								
+				file = File.open("routes.txt","w+")
+				file.write("client = #{@n}\n")
+				write(connection, "que ruta")
+				file.puts(recv(connection)+"\n")
+				file.close
+			end
 			@client.each do | name, socket |
 				if socket.eql? connection
 				else	
